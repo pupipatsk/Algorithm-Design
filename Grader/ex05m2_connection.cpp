8f652edee24b352add438bc3a215878e7baf4bbf
pupipatsk <pupipat.sk@gmail.com>
2024-04-20 19:58:01 +0700

    description

--- Grader/ex05m2_connection.cpp
+++ Grader/ex05m2_connection.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 const int MAX_N = 1005;
-vector<int> adj[MAX_N];
-int dist[MAX_N];
+vector<int> adj[MAX_N]; // graph // adjency list
+int dist[MAX_N]; // distance from present vertex to neighbour
 
 int main() {
     int n, e, k;
@@ -14,6 +14,7 @@ int main() {
 
     while (e--)
     {
+        // create undirected graph
         int a, b;
         cin >> a >> b;
         adj[a].push_back(b);
@@ -23,18 +24,20 @@ int main() {
     int ans = 0;
     for (int i = 0; i < n; i++) {
         int cnt = 1;
-        for (int j = 0; j < n; j++) dist[j] = -1;
+        for (int j = 0; j < n; j++) dist[j] = -1; // reset distance
 
+        // BFS by queue
         queue<int> q;
         q.push(i);
         dist[i] = 0;
+        
         while (!q.empty())
         {
             int u = q.front();
             q.pop();
 
-            if (dist[u] == k) break;
-
+            if (dist[u] >= k) break;
+            // go to next level
             for (auto &v : adj[u]) {
                 if (dist[v] == -1) {
                     dist[v] = dist[u] + 1;
